91af09d084c0a32b8be6b8a05c6c4ef50529a7d0
Max Stein-Golenbock <msteing@gmail.com>
2013-07-24 22:09:20 -0400

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   # callback method to ensure email is lowercase in db
   before_save { self.email = email.downcase }
-
   validates :name, presence: true, length: { maximum: 50 }
-  VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
       format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
